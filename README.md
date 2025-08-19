e099ddadc22b01f5c536eee60f01491453bcbead
serialcoder13 <kiran@modlix.com>
2025-08-19 22:07:37 +0530

    Update .gitignore to include app-specific files and modify README for mobile app JSON structure. Refactor make.py to enhance error handling, streamline mobile app generation, and improve logging for the build process.

--- README.md
+++ README.md
@@ -17,38 +17,29 @@ It is two parts.
         - --password=&lt;password&gt;
         - --conffile=&lt;filepath&gt;
             - if a conf file is provided in the format of properties file that is considered to load all the arguments.
-    1. The list of urls that are required for the 
-    1. The list of configuratble options in application properties for building the applications.
+    1. JSON representation of the Mobile APP service.
     ```json
         {
-            "name": "appbuilder",
-            ...
-            ...
-            "properties": {
-                "defaultPage": "home",
-                ...
-                ...
-                "mobileApps": {
-                    "<UUIDKey1>": {
-                        "key": "<UUIDKey1>",
-                        "name": "<name>",
-                        "description": "<description>",
-                        "startURL": "", //URL of the application to start. Not provided the default page will be loaded under the client's context.
-                        "ios": false, //Default is false. If true generates the ios file.
-                        "android": false, // Default is false. If true generates the android file.
-                        "icon": "", // URL for the 
-                        // when no splash screen is required then the following section is not required.
-                        "splashScreen": {
-                            "image": "", // URL for the image, recommended resolution is 1024x1024 png.
-                            "color": "", // HEX code for the color
-                            "backgroundImage": "", // URL of the Image for the background like a full wall paper
-                            "fullScreen": false, // Default is false
-                            "gravity": "center", // Default is center. "center", "fill", "fitCenter", "centerCrop"
-                            "color_dark": "", // Color in darkmode
-                            "background_image_dark": "", // URL of the Image for the background in dark mode
-                            "image_dark": "" // URL of the Image in dark mode.
-                        }
-                    }
+            "id" : "<generated>"
+            "appCode": "<AppCode>",
+            "clientCode": "<ClientCode>",
+            "details": {
+                "name": "<name>",
+                "description": "<description>",
+                "startURL": "", //URL of the application to start. Not provided the default page will be loaded under the client's context.
+                "ios": false, //Default is false. If true generates the ios file.
+                "android": false, // Default is false. If true generates the android file.
+                "icon": "", // URL for the 
+                // when no splash screen is required then the following section is not required.
+                "splashScreen": {
+                    "image": "", // URL for the image, recommended resolution is 1024x1024 png.
+                    "color": "", // HEX code for the color
+                    "backgroundImage": "", // URL of the Image for the background like a full wall paper
+                    "fullScreen": false, // Default is false
+                    "gravity": "center", // Default is center. "center", "fill", "fitCenter", "centerCrop"
+                    "color_dark": "", // Color in darkmode
+                    "background_image_dark": "", // URL of the Image for the background in dark mode
+                    "image_dark": "" // URL of the Image in dark mode.
                 }
             }
         }
--- scripts/make.py
+++ scripts/make.py
@@ -6,6 +6,7 @@ import requests
 import shutil
 import logging
 import os
+import traceback
 
 # Configure logging
 logging.basicConfig(
@@ -81,10 +82,10 @@ def download_file_withextension(url, path):
         extension = url.split('.')[-1]
         with open(f"{path}.{extension}", 'wb') as f:
             f.write(response.content)
+        logger.info(f"Downloaded file: {url} to {path}.{extension}")
         return extension
     else:
-        logger.error(f"Failed to download file: {url}")
-        exit(1)
+        raise Exception(f"Failed to download file: {url} with status code: {response.status_code}")
 
 args = read_cli_arguments()
 
@@ -111,164 +112,224 @@ logger.info(f"Using server URL: {url_prefix}")
 basic_token = base64.b64encode(f"{args.username}:{args.password}".encode()).decode()
 headers={"Authorization": f"Basic {basic_token}"}
 
-mobileApp = requests.get(
+nextAppResponse = requests.get(
     f"{url_prefix}/api/ui/applications/mobileApps/next",
     headers=headers
-).json()
+)
 
-if 'exceptionId' in mobileApp:
+if nextAppResponse.status_code == 404:
+    logger.info(f"No more apps to build")
+    exit(0)
+
+if nextAppResponse.status_code != 200:
+    logger.error(f"Error: {nextAppResponse.json()}")
+    exit(1)
+
+mobileApp = nextAppResponse.json()
+
+if 'exceptionId' in mobileApp or not 'details' in mobileApp:
     logger.error(f"Error: {mobileApp}")
     exit(1)
 
-logger.info(f"Generating for mobile app: {mobileApp['name']}")
+try :
 
-uuid = shortuuid()
+    requests.post(f"{url_prefix}/api/ui/applications/mobileApps/status/{mobileApp['id']}", headers=headers, json={"status": "IN_PROGRESS"})
 
-logger.info(f"Creating the folder with uuid: {uuid}")
+    logger.info(f"Updated the status to IN_PROGRESS")
+    
+    details = mobileApp['details']
 
-# Copy the ../flutter/nocodemobile to ./{uuid}
-shutil.copytree('../flutter/nocodemobile', f'./{uuid}')
+    logger.info(f"Generating for mobile app: {details['name']}")
 
-# Replace the app title in the app_properties.dart file
-app_properties_file = f'./{uuid}/lib/app_properties.dart'
+    uuid = f"app_{shortuuid()}"
 
-with open(app_properties_file, 'w') as f:
-    f.write(f"class AppProperties {{\n")
-    f.write(f"  static const String appTitle = '{mobileApp['name']}';\n")
-    f.write(f"  static const String homePageTitle = '{mobileApp['name']}';\n")
-    f.write(f"  static const String appVersion = '{mobileApp['version']}.0.0';\n")
-    f.write(f"  static const String startURL = '{mobileApp['startURL']}';\n")
-    f.write(f"}}\n")
+    logger.info(f"Creating the folder with uuid: {uuid}")
 
-logger.info(f"App properties file created: {app_properties_file}")
+    # Copy the ../flutter/nocodemobile to ./{uuid}
+    shutil.copytree('../flutter/nocodemobile', f'./{uuid}')
 
-if mobileApp['icon']:
-    # Download the icon
-    icon_url = f"{url_prefix}/{mobileApp['icon']}"
-    extension = download_file_withextension(icon_url, f'./{uuid}/assets/icon')
-    mobileApp['icon'] = f"assets/icon.{extension}"
+    # Replace the app title in the app_properties.dart file
+    app_properties_file = f'./{uuid}/lib/app_properties.dart'
 
-if 'splashScreen' in mobileApp:
-    if 'image' in mobileApp['splashScreen']:
-        extension = download_file_withextension(f"{url_prefix}/{mobileApp['splashScreen']['image']}", f'./{uuid}/assets/splash_screen')
-        mobileApp['splashScreen']['image'] = f"assets/splash_screen.{extension}"
-    if 'backgroundImage' in mobileApp['splashScreen']:
-        extension = download_file_withextension(f"{url_prefix}/{mobileApp['splashScreen']['backgroundImage']}", f'./{uuid}/assets/splash_screen_background')
-        mobileApp['splashScreen']['backgroundImage'] = f"assets/splash_screen_background.{extension}"
-    if 'image_dark' in mobileApp['splashScreen']:
-        extension = download_file_withextension(f"{url_prefix}/{mobileApp['splashScreen']['image_dark']}", f'./{uuid}/assets/splash_screen_dark')
-        mobileApp['splashScreen']['image_dark'] = f"assets/splash_screen_dark.{extension}"
-    if 'background_image_dark' in mobileApp['splashScreen']:
-        extension = download_file_withextension(f"{url_prefix}/{mobileApp['splashScreen']['background_image_dark']}", f'./{uuid}/assets/splash_screen_background_dark')
-        mobileApp['splashScreen']['background_image_dark'] = f"assets/splash_screen_background_dark.{extension}"
+    with open(app_properties_file, 'w') as f:
+        f.write(f"class AppProperties {{\n")
+        f.write(f"  static const String appTitle = '{details['name']}';\n")
+        f.write(f"  static const String homePageTitle = '{details['name']}';\n")
+        f.write(f"  static const String appVersion = '{details['version']}.0.0';\n")
+        f.write(f"  static const String startURL = '{details['startURL']}';\n")
+        f.write(f"}}\n")
 
-logger.info(f"After all the downloads, the mobileApp is: {mobileApp}")
+    logger.info(f"App properties file created: {app_properties_file}")
 
-# Appending the assets to the pubspec.yaml file
-with open(f'./{uuid}/pubspec.yaml', 'a') as f:
-    f.write(f"\n  assets:\n")
-    if 'icon' in mobileApp:
-        f.write(f"    - {mobileApp['icon']}\n")
+    if details['icon']:
+        # Download the icon
+        icon_url = f"{url_prefix}/{details['icon']}"
+        extension = download_file_withextension(icon_url, f'./{uuid}/assets/icon')
+        details['icon'] = f"assets/icon.{extension}"
 
-    if 'splashScreen' in mobileApp:
-        if 'image' in mobileApp['splashScreen']:
-            f.write(f"    - {mobileApp['splashScreen']['image']}\n")
-        if 'backgroundImage' in mobileApp['splashScreen']:
-            f.write(f"    - {mobileApp['splashScreen']['backgroundImage']}\n")
-        if 'image_dark' in mobileApp['splashScreen']:
-            f.write(f"    - {mobileApp['splashScreen']['image_dark']}\n")
-        if 'background_image_dark' in mobileApp['splashScreen']:
-            f.write(f"    - {mobileApp['splashScreen']['background_image_dark']}\n")
-        
-        f.write(f"flutter_native_splash:\n")
-        if 'color' in mobileApp['splashScreen']:
-            f.write(f"  color: \"{mobileApp['splashScreen']['color']}\"\n")
-        if 'image' in mobileApp['splashScreen']:
-            f.write(f"  image: {mobileApp['splashScreen']['image']}\n")
-        if 'backgroundImage' in mobileApp['splashScreen']:
-            f.write(f"  backgroundImage: {mobileApp['splashScreen']['backgroundImage']}\n")
-        if 'image_dark' in mobileApp['splashScreen']:
-            f.write(f"  image_dark: {mobileApp['splashScreen']['image_dark']}\n")
-        if 'background_image_dark' in mobileApp['splashScreen']:
-            f.write(f"  background_image_dark: {mobileApp['splashScreen']['background_image_dark']}\n")
-        if 'fullScreen' in mobileApp['splashScreen']:
-            value = "true" if mobileApp['splashScreen']['fullScreen'] else "false"
-            f.write(f"  fullscreen: {value}\n")
-        if 'color_dark' in mobileApp['splashScreen']:
-            f.write(f"  color_dark: {mobileApp['splashScreen']['color_dark']}\n")
+    if 'splashScreen' in details:
+        if 'image' in details['splashScreen']:
+            extension = download_file_withextension(f"{url_prefix}/{details['splashScreen']['image']}", f'./{uuid}/assets/splash_screen')
+            details['splashScreen']['image'] = f"assets/splash_screen.{extension}"
+        if 'backgroundImage' in details['splashScreen']:
+            extension = download_file_withextension(f"{url_prefix}/{details['splashScreen']['backgroundImage']}", f'./{uuid}/assets/splash_screen_background')
+            details['splashScreen']['backgroundImage'] = f"assets/splash_screen_background.{extension}"
+        if 'image_dark' in details['splashScreen']:
+            extension = download_file_withextension(f"{url_prefix}/{details['splashScreen']['image_dark']}", f'./{uuid}/assets/splash_screen_dark')
+            details['splashScreen']['image_dark'] = f"assets/splash_screen_dark.{extension}"
+        if 'background_image_dark' in details['splashScreen']:
+            extension = download_file_withextension(f"{url_prefix}/{details['splashScreen']['background_image_dark']}", f'./{uuid}/assets/splash_screen_background_dark')
+            details['splashScreen']['background_image_dark'] = f"assets/splash_screen_background_dark.{extension}"
 
-        gravity = mobileApp['splashScreen']['gravity']
-        if 'android' in mobileApp and mobileApp['android']:
-            f.write(f"  android: true\n")
-            f.write(f"  android_gravity: {gravity}\n")
+    logger.info(f"After all the downloads, the mobileApp is: {details}")
 
-        if 'ios' in mobileApp and mobileApp['ios']:
-            f.write(f"  ios: true\n")
+    # Appending the assets to the pubspec.yaml file
+    with open(f'./{uuid}/pubspec.yaml', 'a') as f:
+        f.write(f"\n  assets:\n")
+        if 'icon' in details:   
+            f.write(f"    - {details['icon']}\n")
 
-            ios_content_mode = {
-                'center': 'center',
-                'fill': 'scaleToFill',
-                'fitCenter': 'scaleAspectFit',
-                'centerCrop': 'scaleAspectFill'
-            }[gravity]
+        if 'splashScreen' in details:
+            if 'image' in details['splashScreen']:
+                f.write(f"    - {details['splashScreen']['image']}\n")
+            if 'backgroundImage' in details['splashScreen']:
+                f.write(f"    - {details['splashScreen']['backgroundImage']}\n")
+            if 'image_dark' in details['splashScreen']:
+                f.write(f"    - {details['splashScreen']['image_dark']}\n")
+            if 'background_image_dark' in details['splashScreen']:
+                f.write(f"    - {details['splashScreen']['background_image_dark']}\n")
+            
+            f.write(f"flutter_native_splash:\n")
+            if 'color' in details['splashScreen']:
+                f.write(f"  color: \"{details['splashScreen']['color']}\"\n")
+            if 'image' in details['splashScreen']:
+                f.write(f"  image: {details['splashScreen']['image']}\n")
+            if 'backgroundImage' in details['splashScreen']:
+                f.write(f"  backgroundImage: {details['splashScreen']['backgroundImage']}\n")
+            if 'image_dark' in details['splashScreen']:
+                f.write(f"  image_dark: {details['splashScreen']['image_dark']}\n")
+            if 'background_image_dark' in details['splashScreen']:
+                f.write(f"  background_image_dark: {details['splashScreen']['background_image_dark']}\n")
+            if 'fullScreen' in details['splashScreen']:
+                value = "true" if details['splashScreen']['fullScreen'] else "false"
+                f.write(f"  fullscreen: {value}\n")
+            if 'color_dark' in details['splashScreen']:
+                f.write(f"  color_dark: {details['splashScreen']['color_dark']}\n")
 
-            if ios_content_mode:
-                f.write(f"  ios_content_mode: {ios_content_mode}\n")
+            gravity = details['splashScreen']['gravity']
+            if 'android' in details and details['android']:
+                f.write(f"  android: true\n")
+                f.write(f"  android_gravity: {gravity}\n")
 
-    f.write(f"version: {mobileApp['version']}.0.0+1\n")
-    lowercasename = mobileApp['name'].lower().replace(' ', '_')
-    f.write(f"name: {lowercasename}\n")
-    if 'description' in mobileApp:
-        f.write(f"description: {mobileApp['description']}\n")
+            if 'ios' in details and details['ios']:
+                f.write(f"  ios: true\n")
 
-    if 'icon' in mobileApp:
-        f.write(f"flutter_icons:\n")
-        if 'android' in mobileApp and mobileApp['android']:
-            f.write(f"  android: true\n")
-        if 'ios' in mobileApp and mobileApp['ios']:
-            f.write(f"  ios: true\n")
-        f.write(f"  image_path: {mobileApp['icon']}\n")
+                ios_content_mode = {
+                    'center': 'center',
+                    'fill': 'scaleToFill',
+                    'fitCenter': 'scaleAspectFit',
+                    'centerCrop': 'scaleAspectFill'
+                }[gravity]
 
-logger.info(f"Pubspec.yaml file updated: {f'./{uuid}/pubspec.yaml'}")
+                if ios_content_mode:
+                    f.write(f"  ios_content_mode: {ios_content_mode}\n")
+
+        f.write(f"version: {details['version']}.0.0+1\n")
+        lowercasename = details['name'].lower().replace(' ', '_')
+        f.write(f"name: {lowercasename}\n")
+        if 'description' in details:
+            f.write(f"description: {details['description']}\n")
+
+        if 'icon' in details:
+            f.write(f"flutter_icons:\n")
+            if 'android' in details and details['android']:
+                f.write(f"  android: true\n")
+            if 'ios' in details and details['ios']:
+                f.write(f"  ios: true\n")
+            f.write(f"  image_path: {details['icon']}\n")
+
+    logger.info(f"Pubspec.yaml file updated: {f'./{uuid}/pubspec.yaml'}")
 
 
-blundle_gradle_kts = f"./{uuid}/android/app/build.gradle.kts"
+    blundle_gradle_kts = f"./{uuid}/android/app/build.gradle.kts"
 
-with open(blundle_gradle_kts, 'r') as f:
-    content = f.read()
+    with open(blundle_gradle_kts, 'r') as f:
+        content = f.read()
 
-client_code_lowercase = mobileApp['clientCode'].lower()
-content = content.replace("applicationId = \"com.modlix.nocodemobile\"", f"applicationId = \"com.modlix.nocodemobile.{client_code_lowercase}.{lowercasename}\"")
+    client_code_lowercase = mobileApp['clientCode'].lower()
+    content = content.replace("applicationId = \"com.modlix.nocodemobile\"", f"applicationId = \"com.modlix.nocodemobile.{client_code_lowercase}.{lowercasename}\"")
 
-with open(blundle_gradle_kts, 'w') as f:
-    f.write(content)
+    with open(blundle_gradle_kts, 'w') as f:
+        f.write(content)
 
-android_manifest = f"./{uuid}/android/app/src/main/AndroidManifest.xml"
+    android_manifest = f"./{uuid}/android/app/src/main/AndroidManifest.xml"
 
-with open(android_manifest, 'r') as f:
-    content = f.read()
+    with open(android_manifest, 'r') as f:
+        content = f.read()
 
-content = content.replace("android:label=\"nocodemobile\"", f"android:label=\"{mobileApp['name']}\"")
+    content = content.replace("android:label=\"nocodemobile\"", f"android:label=\"{details['name']}\"")
 
-with open(android_manifest, 'w') as f:
-    f.write(content)
+    with open(android_manifest, 'w') as f:
+        f.write(content)
 
-ios_info_plist = f"./{uuid}/ios/Runner/Info.plist"
+    ios_info_plist = f"./{uuid}/ios/Runner/Info.plist"
 
-with open(ios_info_plist, 'r') as f:
-    content = f.read()
+    with open(ios_info_plist, 'r') as f:
+        content = f.read()
 
-content = content.replace("<string>Nocodemobile</string>", f"<string>{mobileApp['name']}</string>")
+    content = content.replace("<string>Nocodemobile</string>", f"<string>{details['name']}</string>")
 
-with open(ios_info_plist, 'w') as f:
-    f.write(content)
+    with open(ios_info_plist, 'w') as f:
+        f.write(content)
 
-os.system(f"cd {uuid} && keytool -genkey -v -keystore android/app/keystore/release.keystore -alias modlix -keyalg RSA -keysize 2048 -validity 10000 -storepass modlix_778_231 -keypass modlix_778_231 -dname \"CN=Modlix Team, OU=Mobile Development, O=Modlix, L=Karnataka, ST=Karnataka, C=IN\" -noprompt")
+    os.system(f"cd {uuid} && keytool -genkey -v -keystore android/app/keystore/release.keystore -alias modlix -keyalg RSA -keysize 2048 -validity 10000 -storepass modlix_778_231 -keypass modlix_778_231 -dname \"CN=Modlix Team, OU=Mobile Development, O=Modlix, L=Karnataka, ST=Karnataka, C=IN\" -noprompt")
 
-os.system(f"cd {uuid} && flutter pub get && dart run flutter_native_splash:create && dart run flutter_launcher_icons && dart pub add rename --dev && dart run rename setAppName --value \"{mobileApp['name']}\"")
+    os.system(f"cd {uuid} && flutter pub get && dart run flutter_native_splash:create && dart run flutter_launcher_icons && dart pub add rename --dev && dart run rename setAppName --value \"{details['name']}\"")
 
-if 'android' in mobileApp and mobileApp['android']:
-    os.system(f"cd {uuid} && flutter build appbundle --release")
+    if 'android' in details and details['android']:
+        os.system(f"cd {uuid} && flutter build appbundle --release")
+
+    # Check if the file is built in the output directory {uuid}/build/app/outputs/bundle/release/app-release.aab
+    filePath = f"./{uuid}/build/app/outputs/bundle/release/app-release.aab";
+
+    if not os.path.exists(filePath):
+        requests.post(f"{url_prefix}/api/ui/applications/mobileApps/status/{mobileApp['id']}", headers=headers, json={"status": "FAILED", "errorMessage": "Android build failed. Please, check with Engineering team."})
+        exit(1)
+
+    # Rename the file to <appCode>_<clientCode>_<version>.aab
+    newFileName = f"{mobileApp['appCode']}_{client_code_lowercase}_{details['version']}.aab"
+    newFilePath = f"./{uuid}/build/app/outputs/bundle/release/{newFileName}"
+    os.rename(filePath, newFilePath)
+
+    # Upload the file to the server, using Multipart/form-data
+    fileUploadURL = f"api/files/secured/_withInClient/"
+
+
+    with open(newFilePath, 'rb') as f:
+        response = requests.post(f"{url_prefix}/{fileUploadURL}?clientCode={mobileApp['clientCode']}", headers=headers, files={'file': (newFileName, f, 'application/octet-stream')}, timeout=120)
+
+    if response.status_code != 200:
+        raise Exception(f"Failed to upload the file to the server.To the URL: {fileUploadURL}. Status code: {response.status_code}. Response: {response.json()}")
+
+    logger.info(f"File uploaded to the server. URL: {response.json()['url']}")
+
+    requests.post(f"{url_prefix}/api/ui/applications/mobileApps/status/{mobileApp['id']}", headers=headers, json={
+        "status": "SUCCESS", 
+        "androidAppURL": f"api/files/secured/file/{mobileApp['clientCode']}/_withInClient/{mobileApp['appCode']}_{client_code_lowercase}_{details['version']}.aab",
+    })
+
+    logger.info(f"Updated the status to SUCCESS")
+
+    # Delete the folder
+    shutil.rmtree(uuid)
+    logger.info(f"Deleted the folder: {uuid}")
+
+except Exception as e:    
+    stack_trace = traceback.format_exc()
+    logger.error(f"Error: {e}")
+    logger.error(f"Stack trace: {stack_trace}")
+    requests.post(f"{url_prefix}/api/ui/applications/mobileApps/status/{mobileApp['id']}", headers=headers, json={"status": "FAILED", "errorMessage": str(e)})
+    exit(1)
 
 # if 'ios' in mobileApp and mobileApp['ios']:
 #     os.system(f"cd {uuid} && flutter build ipa --release")
