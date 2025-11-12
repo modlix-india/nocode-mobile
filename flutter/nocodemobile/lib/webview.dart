47d53cc2b209713ca506c0afc7b4f452ddfc5dee
serialcoder13 <kiran@modlix.com>
2025-11-12 09:13:32 +0530

    Remove unnecessary external storage permissions from AndroidManifest.xml and clean up imports in webview.dart to streamline the codebase.

--- flutter/nocodemobile/lib/webview.dart
+++ flutter/nocodemobile/lib/webview.dart
@@ -5,7 +5,6 @@ import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:dio/dio.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter_file_dialog/flutter_file_dialog.dart';
-import 'package:open_filex/open_filex.dart';
 
 class MyWebView extends StatefulWidget {
   final String url;
